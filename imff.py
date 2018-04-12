imff.py fixed numerical instability due to finite integration regime for MFT potentials, added arg parsing for potential parameters for testing

imff.py
@@ -35,6 +35,7 @@ def Aharm(K,beta):
 # DEFINE POTENTIALS
 #------------------------------------------------
 # 1D potentials 
+#------------------------------------------------
 # harmonic
 omega = 1.0
 omega2 = np.power(omega,2)
@@ -47,29 +48,45 @@ a = 3.0
 def ffmorse(q):
   return De * (1 - np.exp(-0.5 * a * q))**2, -a * De * np.exp(-0.5 * a * q)*(1 - np.exp(-0.5 * a * q))
 # double well
-dw = 1.0
-bb = 2.0
+w = -np.sqrt(2.0)
+v = 1.0
+k = -m*np.power(w,2)
+l = m*np.power(v,2)
 def ffdw(q):
-  return  dw * (q**4 - bb * q**2),  dw * (2.0 * bb * q - 4.0 * q**3)
+  return  k * q**2 + l * q**4, -(2.0 * k * q + 4.0 * l * q**3)
 # polynomial
-def ffpoly(q):
-  return q**2 - q**3 + q**4, -2.0 * q + 3.0 * q**2 -4.0 * q**3
+def ffpoly(x, px):
+  return (px[0] * x**2 + px[1] * x**3 + px[2] * x**4 + px[3] * x**6), -(2.0 * px[0] * x + 3.0 * px[1] * x**2 + 4.0 * px[2] * x**3 + 6.0 * px[3] * x**5)
 
+#------------------------------------------------
 # 2D potentials 
+#------------------------------------------------
 # harmonic
-omega1 = 1.0
-omega1squared = np.power(omega1,2)
-omega2 = 1.0
-omega2squared = np.power(omega2,2)
-k1 = m*omega1squared
-k2 = m*omega2squared
+w1 = -np.sqrt(2.0)
+w2 = -np.sqrt(2.0)
+v1 = 1.0
+v2 = 1.0
+k1 = -m*np.power(w1,2)
+k2 = -m*np.power(w2,2)
+l1 = m*np.power(v1,2)
+l2 = m*np.power(v2,2)
 def ffharm2(q):
   return 0.5 * (k1 * np.power(q[0],2) + k2 * np.power(q[1],2)), -k1 * q[0], -k2 * q[1]
+# double double well
+def ffdw2(q):
+  return k1 * q[0]**2 + k2 * q[1]**2 + l1 * q[0]**4 + l2 * q[1]**4, -(2.0 * k1 * q[0] + 4.0 * l1 * q[0]**3), -(2.0 * k2 * q[1] + 4.0 * l2 * q[1]**3)
+# double polynomial
+def ffpoly2(q, px, py):
+  return (px[0] * q[0]**2 + px[1] * q[0]**3 + px[2] * q[0]**4 + px[3] * q[0]**6) + (py[0] * q[1]**2 + py[1] * q[1]**3 + py[2] * q[1]**4 + py[3] * q[1]**6), -1.0 * (2.0 * px[0] * q[0] + 3.0 * px[1] * q[0]**2 + 4.0 * px[2] * q[0]**3 + 6.0 * px[3] * q[0]**5), -1.0 * (2.0 * py[0] * q[1] + 3.0 * py[1] * q[1]**2 + 4.0 * py[2] * q[1]**3 + 6.0 * py[3] * q[1]**5)
 
-  
-#================================================
+
+#================================================================================================
+#================================================================================================
+#================================================================================================
 # IMF (1D)
-#================================================
+#================================================================================================
+#================================================================================================
+#================================================================================================
 
 fqrms = 4.0 # spacing of sampling points in terms of RMS displacements
 nint = 4001 # integration points for numerical integration of Hamiltonian matrix elements
@@ -88,16 +105,17 @@ def psi(n,mass,freq,x):
     psival = norm * np.exp(-nu * x**2 /2.0) * hf[n](np.sqrt(nu)*x)
     return psival
  
-def imf1d(fmode, qeq, K, beta):
+def imf1d(qeq, K, beta, param):
 
   # set potential
-  if (fmode == 'dw'):
-    ff = ffdw
-  elif (fmode == 'harm'):
-    ff = ffharm
+  #if (fmode == 'dw'):
+  #  ff = ffdw
+  #elif (fmode == 'harm'):
+  #  ff = ffharm
+  def ff(x): return ffpoly(x, param)
 
   # evaluate harmonic free energy as reference
-  Ahar = Aharm(K, beta)
+  Ahar = Aharm(np.abs(K), beta)
 
   # evaluate initial harmonic vibrational energy
   if(K > 0):
@@ -226,13 +244,11 @@ def imf1d(fmode, qeq, K, beta):
       hrow = []
       for j in xrange(nbasis):
         dv = np.sum(psi(i,m,whar,ddq-qeqshift) * (vtotspline(ddq) - 0.5*m*whar**2*(ddq-qeqshift)**2) * psi(j,m,whar,ddq-qeqshift)) * (ddq[1] - ddq[0])
-  
         if (i == j):
           hrow.append( (i + 0.5) * hbar * whar + dv )
         else:
           hrow.append( dv )
       h.append(hrow)
-  
   
     # Diagonalise Hamiltonian matrix and evaluate anharmonic free energy and vibrational freq
     evals, evecs = np.linalg.eigh(h)
@@ -390,42 +406,63 @@ def imf1d(fmode, qeq, K, beta):
 
   # write out potentials
   dddq = np.linspace(qmin,qmax,1000)
-  np.savetxt('pot_anh.map.dat',np.c_[qtots,vs])
-  np.savetxt('pot_tot.map.dat',np.c_[qtots,vtots])
-  np.savetxt('pot_anh.fit.dat',np.c_[dddq,vspline(dddq)])
-  np.savetxt('pot_tot.fit.dat',np.c_[dddq,vtotspline(dddq)])
+  #np.savetxt('pot_anh.map.dat',np.c_[qtots,vs])
+  #np.savetxt('pot_tot.map.dat',np.c_[qtots,vtots])
+  #np.savetxt('pot_anh.fit.dat',np.c_[dddq,vspline(dddq)])
+  #np.savetxt('pot_tot.fit.dat',np.c_[dddq,vtotspline(dddq)])
   # write out wvfn
-  np.savetxt('psi_gs.dat',np.c_[dddq-qeqshift,psi(0,m,whar,dddq-qeqshift)])
-  np.savetxt('psi.dat',np.c_[dddq-qeqshift, np.sum(np.asarray([psi(jj,m,whar,dddq-qeqshift) * evecs.T[0][jj] for jj in xrange(nbasis)]),axis=0) ])
+  #np.savetxt('psi_gs.dat',np.c_[dddq-qeqshift,psi(0,m,whar,dddq-qeqshift)])
+  #np.savetxt('psi.dat',np.c_[dddq-qeqshift, np.sum(np.asarray([psi(jj,m,whar,dddq-qeqshift) * evecs.T[0][jj] for jj in xrange(nbasis)]),axis=0) ])
   # write convergence to logfile
-  np.savetxt('log.imff.'+fmode+'.'+str(qeq)+'.'+str(K)+'.'+str(beta)+'.dat',np.c_[qmins,qmaxs,npoints,Ahars,Aanhs,Adiffs], fmt='%6.3f %6.3f %3i % 12.6f % 12.6f % 12.6f')
+  #np.savetxt('log.imff.'+fmode+'.'+str(qeq)+'.'+str(K)+'.'+str(beta)+'.dat',np.c_[qmins,qmaxs,npoints,Ahars,Aanhs,Adiffs], fmt='%6.3f %6.3f %3i % 12.6f % 12.6f % 12.6f')
+
+
+  # Print 2-body energies E for debugging
+  #print E
+  #for i in range(10):
+  #  print evals[i]
+
+  E = np.zeros((10,10))
+  for s0 in xrange(10):
+    for s1 in xrange(10):
+      E[s0][s1] = evals[s0] + evals[s1]
+
+  print E[0][0]
+
+  # Calculate total partition function and free energy for current iteration iiter
+  Z = np.sum(np.exp(-beta*E))
+  A = -np.log(Z)/beta
 
   # done
   return Ahars[-1],Aanhs[-1],Adiffs[-1]
   
 
 
-#================================================
+#================================================================================================
+#================================================================================================
+#================================================================================================
 # IMF (2D)
-#================================================
+#================================================================================================
+#================================================================================================
+#================================================================================================
 
-fqrms = 4.0 # spacing of sampling points in terms of RMS displacements
+fqrms = 0.5 # spacing of sampling points in terms of RMS displacements
 nint = 101 # integration points for numerical integration of Hamiltonian matrix elements
 nbasis = 10 # number of SHO states used as basis for anharmonic wvfn
 nenergy = 10.0 # potential mapped until v - v_eq > nenergy * ehar
 wthresh = 1e-3
 
-def imf2d(beta):
+def imf2d(qeq, K, beta, parx, pary):
 
   # initialise harmonic equilibrium positions and Hessian
-  qeq = np.zeros(2)
-  K = np.ones(2)
+  #qeq = np.zeros(2)
+  #K = np.ones(2)
 
   # set potential
-  ff = ffharm2
+  def ff(q): return ffpoly2(q, parx, pary)
 
   # evaluate harmonic free energy as reference
-  Ahar = Aharm(K[0], beta) + Aharm(K[1], beta)
+  Ahar = Aharm(np.abs(K[0]), beta) + Aharm(np.abs(K[1]), beta)
 
   # evaluate initial harmonic vibrational energy
   whar = np.zeros(len(qeq))
@@ -477,6 +514,10 @@ def imf2d(beta):
         qmin[i] = -j*dq[i]
         break
 
+  if (min(npts) <= 3):
+    print "Fewer than three sampling points along one axis -- cannot fit cubic spline"
+    print edgar
+
   wanh = []
   wanh.append(whar)
 
@@ -487,26 +528,38 @@ def imf2d(beta):
   vtots = np.zeros(npts[0]*npts[1])
   fs = np.zeros((npts[0]*npts[1],len(qeq)))
 
-  i = -1
+  k=-1
   q[0] = qmin[0]
-  while (q[0] <= qmax[0]):
+  for i in range(npts[0]):
     q[1] = qmin[1]
-    while (q[1] <= qmax[1]):
-      i += 1
+    for j in range(npts[1]):
+      k += 1
       v,f[0],f[1] = ff(qeq+q)
       # store positions, potentials, and forces in arrays
-      qs[i] = q
-      qtots[i] = qeq+q
-      vs[i] = v - 0.5*m*((whar[0]*q[0])**2 + (whar[1]*q[1])**2) - ff(qeq)[0]
-      vtots[i] = v - ff(qeq)[0]
-      fs[i] = f
+      qs[k] = q
+      qtots[k] = qeq+q
+      vs[k] = v - 0.5*m*((whar[0]*q[0])**2 + (whar[1]*q[1])**2) - ff(qeq)[0]
+      vtots[k] = v - ff(qeq)[0]
+      fs[k] = f
       # done storing
       q[1] += dq[1]
     q[0] += dq[0]
-     
+
   # fit 2D cubic spline to sampled potential
-  vspline = interp2d(np.asarray(qs.T[0]), np.asarray(qs.T[1]), np.asarray(vs), kind='cubic', bounds_error=False)
   vtotspline = interp2d(np.asarray(qs.T[0]), np.asarray(qs.T[1]), np.asarray(vtots), kind='cubic', bounds_error=False)
+
+#  # print fitted potential
+#  for q0 in np.linspace(qmin[0],qmax[0],21):
+#    for q1 in np.linspace(qmin[1],qmax[1],21):
+#      print q0,q1,vtotspline(q0,q1)
+#  # print sampled potential
+#  iv=0
+#  for i0 in range(npts[0]):
+#    for i1 in range(npts[1]):
+#      print np.asarray(qs.T[0])[iv], np.asarray(qs.T[1])[iv], np.asarray(vtots)[iv]
+#      iv+=1
+#  # bail out
+#  print edgar
 
   #-------------------------------------------------------
   # solve the VSCF problem
@@ -583,6 +636,7 @@ def imf2d(beta):
 
   Z = np.sum(np.exp(-beta*E)) # harmonic partition function
   A = -np.log(Z)/beta # harmonic free energy
+  A += ff(qeq)[0]
   Aold = A
 
   ddq = []
@@ -600,21 +654,36 @@ def imf2d(beta):
     for s0 in xrange(nbasis):
       for s1 in xrange(nbasis):
 
-        print "Calc. ",s0,"/",nbasis," ",s1,"/",nbasis
+        #print "Calc. ",s0,"/",nbasis," ",s1,"/",nbasis
 
         for k in range(nint):
           dddq = ddq[0][k]
           # Wvfn of mode 1 in state s1 given that mode 0 is in state s0
           psitmp = np.sum([psi(basis,m,whar[1],ddq[1])*evecs[1][s0][s1][basis] for basis in xrange(nbasis)],axis=0)
+          normtmp = np.sum(np.asarray([ psitmp[jj]**2 for jj in xrange(nint)]),axis=0)
           # MF potential that mode 0 lives in given that mode 1 is in state s1 with wvfn psitmp
-          vmode[0][s1][s0][k] = np.sum(np.asarray([ ((vtotspline(dddq,ddq[1][jj]) - 0.5 * (whar[1]*ddq[1][jj])**2) * psitmp[jj]**2 * (ddq[1][1]-ddq[1][0])) for jj in xrange(nint)]),axis=0)
+          vmode[0][s1][s0][k] = np.sum(np.asarray([ (vtotspline(dddq,ddq[1][jj]) * psitmp[jj]**2 / normtmp) for jj in xrange(nint)]),axis=0)
+        kqeq = np.argmin(ddq[0]**2)
+        vmode[0][s1][s0] -= np.ones(nint) * vmode[0][s1][s0][kqeq]
 
         for k in range(nint):
           dddq = ddq[1][k]
           # Wvfn of mode 0 in state s0 given that mode 1 is in state s1
           psitmp = np.sum([psi(basis,m,whar[0],ddq[0])*evecs[0][s1][s0][basis] for basis in xrange(nbasis)],axis=0)
+          normtmp = np.sum(np.asarray([ psitmp[jj]**2 for jj in xrange(nint)]),axis=0)
           # MF potential that mode 1 lives in given that mode 0 is in state s0 with wvfn psitmp
-          vmode[1][s0][s1][k] = np.sum(np.asarray([ ((vtotspline(ddq[0][jj],dddq) - 0.5 * (whar[0]*ddq[0][jj])**2) * psitmp[jj]**2 * (ddq[0][1]-ddq[0][0])) for jj in xrange(nint)]),axis=0)
+          vmode[1][s0][s1][k] = np.sum(np.asarray([ (vtotspline(ddq[0][jj],dddq) * psitmp[jj]**2 / normtmp) for jj in xrange(nint)]),axis=0)
+        kqeq = np.argmin(ddq[1]**2)
+        vmode[1][s0][s1] -= np.ones(nint) * vmode[1][s0][s1][kqeq]
+
+        #if (s0==1):
+        #  if (s1==1):
+        #    print ddq[0]
+        #    print vtotspline(ddq[0],0.0)
+        #    print vmode[0][1][1]
+        #    print ddq[0][nint-1], vtotspline(ddq[0][nint-1],0.0), vmode[0][0][0][nint-1],vmode[0][1][1][nint-1]
+        #    print ddq[1][nint-1], vtotspline(0.0,ddq[1][nint-1]), vmode[1][0][0][nint-1],vmode[1][1][1][nint-1]
+        #    print venkat
 
         # mode 0 in state s0 and mode 1 in state s1
         for i in xrange(nbasis):
@@ -641,29 +710,48 @@ def imf2d(beta):
         dE = np.sum(np.asarray([[((vtotspline(ddq[0][k],ddq[1][l]) - vmode[0][s1][s0][k] - vmode[1][s0][s1][l]) * psicurr[0][s1][k]**2 * psicurr[1][s0][l]**2) for k in xrange(nint)] for l in xrange(nint)]))
         E[s0][s1] = evals[0][s1][s0] + evals[1][s0][s1] + dE
 
+        #if (s0==1):
+        #  if (s1==1):
+        #    for k in [45,46,47,48,49,50,51,52,53,54,55]:
+        #      for l in [45,46,47,48,49,50,51,52,53,54,55]:
+        #        print ddq[0][k],ddq[1][l],(vtotspline(ddq[0][k],ddq[1][l]) - vmode[0][s1][s0][k] - vmode[1][s0][s1][l])
+        #    print edgar
+
+
+        # Print 2-body energies E for debugging
+        #print s0,s1,evals[0][s1][s0],evals[1][s0][s1],dE,E[s0][s1]
+
     # Print 2-body energies E for debugging
-    print E
+    #print E
+    s0 = 0
+    s1 = 0
+    basis = nbasis - 1
+    print E[s0][s1]
+    print evecs[0][s1][s0][basis],evecs[1][s0][s1][basis]
 
     # Calculate total partition function and free energy for current iteration iiter
     Z = np.sum(np.exp(-beta*E))
     A = -np.log(Z)/beta
+    A += ff(qeq)[0]
 
     print "Iteration ",iiter, "Anh. free energy: ",A
     if (abs(A - Aold)/abs(Aold) < 1e-3): break
     Aold = A
 
-    ###########################
+    ######################################################
 
 
   return
 
   
 
-#================================================
-#================================================
+#================================================================================================
+#================================================================================================
+#================================================================================================
 # MAIN
-#================================================
-#================================================
+#================================================================================================
+#================================================================================================
+#================================================================================================
 
 #parser = argparse.ArgumentParser()
 #parser.add_argument("pot", help="type of potential [harm, dw, morse]", type=str)
@@ -673,5 +761,40 @@ def imf2d(beta):
 #args = parser.parse_args()
 #print imf1d(args.pot,args.qeq,args.Keq,args.invT)
 
-imf2d(1.0)
+parser = argparse.ArgumentParser()
+
+parser.add_argument("px1", help="par px1 of the ffpoly2", type=float)
+parser.add_argument("px2", help="par px2 of the ffpoly2", type=float)
+parser.add_argument("px3", help="par px3 of the ffpoly2", type=float)
+parser.add_argument("px4", help="par px4 of the ffpoly2", type=float)
+parser.add_argument("py1", help="par py1 of the ffpoly2", type=float)
+parser.add_argument("py2", help="par py2 of the ffpoly2", type=float)
+parser.add_argument("py3", help="par py3 of the ffpoly2", type=float)
+parser.add_argument("py4", help="par py4 of the ffpoly2", type=float)
+parser.add_argument("invT", help="inverse temperature", type=float)
+args = parser.parse_args()
+
+px = np.asarray([args.px1, args.px2, args.px3, args.px4])
+py = np.asarray([args.py1, args.py2, args.py3, args.py4])
+invT = args.invT
+
+qxtr = np.linspace(-10,10,1e4)
+qxeq = qxtr[np.argmin(ffpoly(qxtr, px)[0])]
+Kxeq = 2 * px[0] + 6 * px[1] * qxeq + 12 * px[2] * qxeq**2 + 30 * px[3] * qxeq**4
+
+qytr = np.linspace(-10,10,1e4)
+qyeq = qytr[np.argmin(ffpoly(qytr, py)[0])]
+Kyeq = 2 * py[0] + 6 * py[1] * qyeq + 12 * py[2] * qyeq**2 + 30 * py[3] * qyeq**4
+
+qxeq = 0.0
+qyeq = 0.0
+Kxeq = 2 * px[0] + 6 * px[1] * qxeq + 12 * px[2] * qxeq**2 + 30 * px[3] * qxeq**4
+Kyeq = 2 * py[0] + 6 * py[1] * qyeq + 12 * py[2] * qyeq**2 + 30 * py[3] * qyeq**4
+
+print "1D results"
+print imf1d(qxeq, Kxeq, invT, px)
+print imf1d(qyeq, Kyeq, invT, py)
+
+print "2D result"
+print imf2d([qxeq, qyeq], [Kxeq, Kyeq], invT, px, py)
 